f206ff06fc568b870431170d9f08ab65974b5718
Saeedkpi <info.saeedbpsc.com>
2025-01-09 22:32:02 +0600

    Access element of Dictionary

---
+++ Dictionary Structure/access_item.py
@@ -0,0 +1,9 @@
+student_information = {
+    "std_id1" : {"Name" : "Md. Jahanggir Alom", "District" : "Dinajpur"},
+    "std_id2" : {"Name" : "Mst. Mim Akter", "District" : "Rangpur"},
+    "std_id3" : {"Name" : "Md. Foridul Islam", "District" : "Rangpur"},
+    "std_id4" : {"Name" : "Sumon Roy", "District" : "Nilfamari"},
+    "std_id5" : {"Name" : "Mst. Aporna Akter", "District" : "Dinajpur"}
+}
+
+print(student_information["std_id2"])
